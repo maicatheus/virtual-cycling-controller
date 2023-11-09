1c95509f9af5b6aa8146bdaa2c9a47ea19b168e3
Matheus <maicatheus@gmail.com>
2023-11-08 23:00:04 -0300

    Steering control

--- VirtualCyclingController.py
+++ VirtualCyclingController.py
@@ -34,6 +34,10 @@ class VirtualCyclingController:
     def midpoint(point1, point2):
         return (int((point1[0] + point2[0]) / 2), int((point1[1] + point2[1]) / 2))
 
+    @staticmethod
+    def normalize_angle(angle,a_max,a_min):
+        return((abs(angle)- a_min)/(a_max-a_min))
+    
     def control_pedaling(self, hand, shoulder):
         if hand[1] < shoulder[1]:
             self.gamepad.left_trigger_float(0.8)
@@ -51,6 +55,19 @@ class VirtualCyclingController:
         else:
             self.gamepad.release_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER)
             self.gamepad.release_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+
+
+    
+    def steering_control(self, angle):
+        a_min, a_max = 1, 30
+        angle = max(min(angle, a_max), -a_max)
+        abs_angle = abs(angle)
+        joystick_value = -0.3 if abs_angle <= 5 else -0.4 if abs_angle <= 12 else -0.5 if abs_angle <= 15 else -0.58
+        
+        if angle != 0:
+            joystick_value *= angle / abs_angle
+        
+        self.gamepad.left_joystick_float(joystick_value, 0)
 
     def control_rotation(self, distance, body_presence):
         try:
@@ -106,6 +123,7 @@ class VirtualCyclingController:
                     angle = self.calculate_angle(mid_point, body[0])
 
                     self.control_pedaling(body[19], body[11])
+                    self.steering_control(angle)
 
                     cv2.putText(frame, f"Distance: {distance} RPM: {int(self.rpm)} Angle: {angle}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
                     cv2.line(frame,body[28],body[24],(255,0,0),2)
